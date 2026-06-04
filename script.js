c6e57581d78e2f45ada394697f97095234c5f24d
Casey <tibedebacker18@gmail.com>
2026-06-04 21:05:11 +0200

    made more mobile friendly

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    document.getElementById("hamburger").addEventListener("click", function(){
+        document.getElementById("hamburgerMenu").classList.remove('hidden');
+    })
+    document.getElementById("hamburgerMenu").addEventListener("click", function() {
+        document.getElementById("hamburgerMenu").classList.add("hidden");
+    })
 });
