53138488404884ef71d0103a358f7cd2b0ba0c9a
Casey <tibedebacker18@gmail.com>
2026-06-05 09:59:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("hamburger").addEventListener("click", function(){
-        document.getElementById("hamburgerMenu").classList.remove('hidden');
+        document.getElementById("hamburgerMenu").classList.remove('hidden'); 
+        document.getElementById("ham").classList.add("hidden");
     })
     document.getElementById("hamburgerMenu").addEventListener("click", function() {
         document.getElementById("hamburgerMenu").classList.add("hidden");
+        document.getElementById("ham").classList.remove('hidden'); 
     })
 });
