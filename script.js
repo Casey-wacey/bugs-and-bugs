2851b1594fbe03051dc81ceecf234b811825996f
Casey <tibedebacker18@gmail.com>
2026-06-05 14:17:34 +0200

    nuno reference?

--- script.js
+++ script.js
@@ -8,4 +8,13 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("hamburgerMenu").classList.add("hidden");
         document.getElementById("ham").classList.remove('hidden'); 
     })
+    document.getElementById("choei").addEventListener("mouseover", function(){
+        document.getElementById("choei").src = "images/choeidance.gif"; 
+    })
+    document.getElementById("choei").addEventListener("mouseout", function(){
+        document.getElementById("choei").src = "images/choei.png"; 
+    })
+    document.getElementById("choei").addEventListener("click", function() {
+        location.href = 'https://nunoke123.github.io/';
+    })
 });
