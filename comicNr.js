7832ef4d7cd9d09ba8acecf31d91aebe1ac52a44
Casey <tibedebacker18@gmail.com>
2026-06-08 11:57:32 +0200

    added some text and did some other things

--- comicNr.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("episodes.json")
-    .then(r => r.json())
-    .then(data => {
-      document.getElementById("episode-count").textContent = data.count;
-    });
-})
